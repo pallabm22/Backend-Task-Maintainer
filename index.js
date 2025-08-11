09cc5322f9ae57864eeef070759bc3df89bd0243
pallabm22 <pallabmandal002@gmail.com>
2025-08-11 20:09:21 +0530

    Changed

--- index.js
+++ index.js
@@ -25,6 +25,12 @@ app.post("/create", (req, res) => {
   });
 });
 
+app.get("/files/:filename", (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
+    res.render("show",{filename:req.params.filename,filedata:filedata});
+  })
+});
+
 app.listen(3000, () => {
   console.log("App is running successfully");
   console.log("App is running without any problem");
