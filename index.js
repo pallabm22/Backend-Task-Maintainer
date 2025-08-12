a1f1295f746f6cffe4025535bebb81efe0b52ca6
pallabm22 <pallabmandal002@gmail.com>
2025-08-12 18:26:13 +0530

    Updated files

--- index.js
+++ index.js
@@ -29,6 +29,36 @@ app.get("/files/:filename", (req, res) => {
   fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
     res.render("show",{filename:req.params.filename,filedata:filedata});
   })
+}); 
+
+app.get("/edit/:filename", (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, filedata) => {
+    if (err) return res.status(500).send("File not found");
+    res.render("edit", {
+      filename: req.params.filename,
+      filedata: filedata,
+    });
+  });
+});
+app.post("/edit", (req, res) => {
+  const oldPath = `./files/${req.body.previous}`;
+  const newFileName = req.body.New.endsWith(".txt")
+    ? req.body.New
+    : req.body.New + ".txt";
+  const newPath = `./files/${newFileName}`;
+  const newDetails = req.body.details;
+
+  fs.rename(oldPath, newPath, (err) => {
+    console.log(err);
+    return res.status(500).send("Error renaming file");
+  });
+  fs.writeFile(newPath, newDetails, (err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send("Error in changing the task details");
+    }
+    res.redirect('/');
+  });
 });
 
 app.listen(3000, () => {
